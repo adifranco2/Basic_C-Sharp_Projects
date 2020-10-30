92ae7b00d6a25f8eba3f32c58f44f7557a6d7ecd
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-10-29 21:01:07 -0400

    upload step 168

---
+++ step168/step168/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step168
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool qualified = (age > 15 && DUI == false && tickets <= 3);
+            Console.WriteLine("Qualified? \n" + qualified);
+            Console.ReadLine();
+        }
+    }
+}
