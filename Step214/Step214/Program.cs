ee6d0e61d20a4fc5c7785157d8538919fefc41c6
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-09 14:11:18 -0500

    upload step 214

---
+++ Step214/Step214/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step214
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numList = new List<int>() { 15, 27, 32, 14};
+
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                
+                foreach (int num in numList)
+                {
+                    int numberTwo = num / numberOne;
+                    Console.WriteLine("The list integer divided by your number equals " + numberTwo);
+                   
+                }   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+           
+            finally
+            {
+                Console.WriteLine("You have emerged from the try/catch block.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
