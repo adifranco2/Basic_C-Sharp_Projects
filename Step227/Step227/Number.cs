799497b0852f99997b1161c12ea36b1d0968636a
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-10 15:48:48 -0500

    upload step 227

---
+++ Step227/Step227/Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step227
+{
+    public class Number
+    {
+        public int Addition(int num1, int num2 = 0)
+        {
+            int result;
+
+            result = num1 + num2 + 2;
+
+            return result;
+        }
+    }
+}
---
+++ Step227/Step227/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("pick a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            int num2;
+
+            Console.WriteLine("you can pick another number but you don't have to"); 
+            string selection = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(selection))
+            {
+                num2 = 0;
+            }
+            else
+            {
+                num2 = Convert.ToInt32(selection);
+            }
+
+                       
+            int ret;
+            Number n = new Number();
+
+            ret = n.Addition(num1, num2);
+            Console.WriteLine("your number(s) plus 2 equals " + ret);
+            Console.ReadLine();
+        }
+    }
+}
