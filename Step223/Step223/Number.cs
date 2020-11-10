aa4bd6350d947e178bfe3ddd85a6aed8b29dd0bc
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-10 11:52:13 -0500

    upload step 223

---
+++ Step223/Step223/Number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step223
+{
+    public class Number
+    {
+        public int Addition(int num1)
+        {
+            int result;
+
+            result = num1 + 2;
+
+            return result;
+        }
+
+        public int Multiplication(int num1)
+        {
+            int result;
+
+            result = num1 * 2;
+
+            return result;
+        }
+
+        public int Subtraction(int num1)
+        {
+            int result;
+
+            result = num1 - 1;
+
+            return result;
+        }
+    }
+}
---
+++ Step223/Step223/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number.");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int ret;
+            Number n = new Number();
+
+            ret = n.Addition(num1);
+            Console.WriteLine("your number plus two equals " + ret);
+
+            ret = n.Multiplication(num1);
+            Console.WriteLine("your number multiplied by two equals " + ret);
+
+            ret = n.Subtraction(num1);
+            Console.WriteLine("your number minus one equals " + ret);
+            Console.ReadLine();
+
+            }
+        
+    }
+}
