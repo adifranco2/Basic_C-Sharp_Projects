a017efbabd55f73bfa6b54d93d589aa39e4862dd
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-11 20:57:16 -0500

    upload step 231

---
+++ Step231/Step231/Number.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step231
+{
+    public class Number
+    {
+        public void Division(int num1)
+        {
+            int result;
+
+            result = num1 / 2;
+
+            Console.WriteLine("your number divided by 2 equals " + result);
+
+            return;
+        }
+
+        public void valueOutput(out int x)
+        {
+            x = 10;
+            x += x;
+        }
+
+        public static int Addition(int a)
+        {
+            int result;
+            result = a + 10;
+            return result;
+        }
+
+        public static double Addition(double b)
+        {
+            double result2;
+            result2 = b + 2.5;
+            return result2;
+        }
+    }
+}
---
+++ Step231/Step231/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pick a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Number n = new Number();
+
+            n.Division(num1);
+
+            Console.WriteLine("output parameters");
+            int x;
+
+            n.valueOutput(out x);
+                 
+            Console.WriteLine("The addition of the value is: {0}", x);
+
+            Console.WriteLine("pick another number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int ret;
+            ret = Step231.Number.Addition(a);
+            Console.WriteLine("your number plus 10 equals " + ret);
+
+            Console.WriteLine("pick one more number");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double ret2;
+            ret2 = Step231.Number.Addition(b);
+            Console.WriteLine("your number plus 2.5 equals " + ret2);
+
+            Console.ReadLine();
+        }
+
+            
+
+    }
+}
