3c30a950413d23292e34ec4740f3a69028c7615f
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-11 19:26:53 -0500

    upload step 228

---
+++ Step228/Step228/Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step228
+{
+    public class Number
+    {
+        public void Addition(int num1, int num2)
+        {
+            int result;
+
+            result = num1 + 2;
+
+            return;
+        }
+    }
+}
---
+++ Step228/Step228/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pick a number");
+            int int1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("pick another number");
+            int int2 = Convert.ToInt32(Console.ReadLine());
+
+            int ret;
+
+            Number n = new Number();
+
+            n.Addition(num1: int1, num2: int2);
+            Console.WriteLine(int2);
+            Console.ReadLine();
+        }
+    }
+}
