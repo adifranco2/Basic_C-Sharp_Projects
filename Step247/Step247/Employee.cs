d6cd860bd5b3ee1dc9d86a91561dd96d3b23fe07
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-14 13:00:38 -0500

    upload step 247

---
+++ Step247/Step247/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step247
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+                Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine(firstName + " " + lastName + " quit thier job last week.");
+        }
+    }
+}
---
+++ Step247/Step247/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step247
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            employee.Quit();
+            Console.ReadLine();
+        }
+    }
+}
