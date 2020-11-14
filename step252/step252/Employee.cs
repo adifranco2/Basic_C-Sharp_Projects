29958ed56ed560c1697bf32d08a20fb31a01f677
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-14 15:59:24 -0500

    upload incomplete step 252

---
+++ step252/step252/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step252
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        
+
+        public static bool operator== (Employee employee, Employee employee1)
+        {
+            if (ReferenceEquals(employee.Id, employee1.Id))
+            {
+                return true;
+            }
+            if (ReferenceEquals(employee.Id, null))
+            {
+                return false;
+            }
+            if (ReferenceEquals(employee1.Id, null))
+            {
+                return false;
+            }
+            return employee.Id.Equals(employee1.Id);
+        }
+        public static bool operator!= (Employee employee, Employee employee1)
+        {
+            return !(employee.Id == employee1.Id);
+        }
+
+    }
+}
---
+++ step252/step252/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step252
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { Id = 1, firstName = "Steve" };
+            Employee employee1 = new Employee() { Id = 2, firstName = "Jane"};
+
+            ReferenceEquals(employee.Id, employee1.Id);
+            Console.WriteLine();
+
+            
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
