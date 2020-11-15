83aed19f03b5820bff3313321366ab04a86529c4
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-14 20:28:45 -0500

    upload step 255

---
+++ step255/step255/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step255
+{
+    public class Employee<T>
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ step255/step255/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step255
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "purple", "clover", "salami" };
+
+            Console.WriteLine("string list:");
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Things = new List<int>() { 2, 5, 13 };
+
+            Console.WriteLine("int list:");
+            foreach (int thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
