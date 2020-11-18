7ad662fa482532700bc18bf8f6d13d4168bbc595
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-17 20:38:05 -0500

    upload step 261

---
+++ Step261/Step261/Days.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step261
+{
+    public class Days
+    {
+        public enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
---
+++ Step261/Step261/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step261
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Please endter the current day of the week.");
+            string enteredDay = Console.ReadLine();
+
+            try
+            {
+                Days.DaysOfWeek day = (Days.DaysOfWeek)Enum.Parse(typeof(Days.DaysOfWeek), enteredDay);
+                Console.WriteLine("You entered " + day);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                
+            }
+
+            Console.ReadLine();
+                       
+        }
+    }
+}
