c57469c48ac4f4a409134a79d3a7d5ddb0c40ea6
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-20 21:49:37 -0500

    upload step 300

---
+++ step300/step300/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine("Currently it is: " + rightNow);
+            Console.WriteLine("Please enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In " + number + " hours, it will be: " + rightNow.AddHours(number));
+            Console.ReadLine();
+        }
+    }
+}
