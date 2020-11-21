d28bb4f5b8fc2e828a0d6fd3f9fdb8619be836bd
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-20 21:25:40 -0500

    upload step 297

---
+++ step297/step297/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace step297
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter a number.");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Amy\Desktop\Basic_C#_Programs\log.txt", text);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Amy\Desktop\Basic_C#_Programs\log.txt"));
+            Console.ReadLine();
+            
+        }
+    }
+}
