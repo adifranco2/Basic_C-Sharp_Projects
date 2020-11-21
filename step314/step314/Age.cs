2e26fda356786b82ca8bb39855e7203d3b2e3140
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-11-21 12:37:46 -0500

    upload step 314

---
+++ step314/step314/Age.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step314
+{
+    class Age
+    {
+        public class valueException : Exception
+        {
+           public valueException()
+                : base() { }
+           public valueException(string message)
+                :base (message) { }
+        }
+    }
+}
---
+++ step314/step314/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //bool validAge = false;
+                //int age = 0;
+                //while (!validAge)
+                //{
+                Console.WriteLine("What is your age?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                DateTime today = DateTime.Now;
+                DateTime birthYear = today.AddYears(-age);
+                //validAge = int.TryParse(Console.ReadLine(), out age);
+                //}
+                //if (!validAge)
+                //{
+                //    throw new Exception();
+                //    //Console.WriteLine("Please enter digits only.")
+                //}
+                if (age <= 0)
+                {
+                    throw new Age.valueException();
+                }
+                else
+                {
+                    Console.WriteLine("Your birth year is " + birthYear.Year);
+                    Console.ReadLine();
+                }
+            }
+            catch (Age.valueException)
+            {
+                Console.WriteLine("Your age cannot be less than 1.");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error has occurred.");
+                Console.ReadLine();
+                
+            }
+
+        }
+    }
+}
